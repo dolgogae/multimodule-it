c9612536a70c736ebe57acabca52ea3f96902466
sihun oh <ohsh0731@gmail.com>
2025-01-01 18:41:53 +0900

    feat: add test module(mmit-integration-test)

---
+++ mmit-integration-test/src/main/java/com/example/mmitintegrationtest/MmitIntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mmitintegrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmitIntegrationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmitIntegrationTestApplication.class, args);
+    }
+
+}
---
+++ mmit-integration-test/src/test/java/com/example/mmitintegrationtest/MmitIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmitintegrationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmitIntegrationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
